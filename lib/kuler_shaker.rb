6c86aba9fbe9cd2fb480e025fe3d527f55ecf3a3
Nick del Pozo <nickdelpozo@gmail.com>
2009-11-05 17:01:00 +1100

    first upload of the kuler shaker lib files

---
+++ lib/kuler_shaker.rb
@@ -0,0 +1,8 @@
+# KulerShaker
+require 'net/http'
+require 'uri'
+require 'rexml/document'
+require File.dirname(__FILE__) + '/kuler_shaker/kuler_shaker'
+require File.dirname(__FILE__) + '/kuler_shaker/kuler_scheme'
+require File.dirname(__FILE__) + '/kuler_shaker/kuler_swatch'
+require File.dirname(__FILE__) + '/kuler_shaker/kuler_comment'
---
+++ tasks/kuler_shaker_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :kuler_shaker do
+#   # Task goes here
+# end
